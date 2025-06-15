841d4c7911478b43a52683ab7baf3c69b9e48a61
oliwia <24637207+kkanden@users.noreply.github.com>
2025-06-15 02:14:16 +0200

    remove workspace from focus-or-launch

--- scripts/focus-or-launch.sh
+++ scripts/focus-or-launch.sh
@@ -2,13 +2,11 @@
 
 execCommand=$1
 className=$2
-workspaceOnStart=$3
 
 running=$(hyprctl -j clients | jq -r ".[] | select(.class == \"${className}\")")
 
 if [[ $running != "" ]]; then
 	hyprctl dispatch focuswindow "class:$className"    
 else 
-	hyprctl dispatch workspace "$workspaceOnStart" 
-	${execCommand} & 
+	hyprctl dispatch "${execCommand}" & 
 fi
