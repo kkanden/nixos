a8e8444e604cef35b84619c20e2abf3180ba68ab
oliwia <24637207+kkanden@users.noreply.github.com>
2025-07-30 22:47:39 +0200

    use stable for oh-my-posh

--- modules-hm/oh-my-posh.nix
+++ modules-hm/oh-my-posh.nix
@@ -1,7 +1,8 @@
-{ lib', ... }:
+{ lib', pkgs, ... }:
 {
   programs.oh-my-posh = {
     enable = true;
+    package = pkgs.stable.oh-my-posh;
     enableBashIntegration = false;
     enableFishIntegration = true;
     settings = builtins.fromJSON (lib'.readConfig "oh-my-posh/omp-vague.json");
