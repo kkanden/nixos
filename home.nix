9743c4bf0c18569c30147adc3a2a3a6e2841f69b
oliwia <24637207+kkanden@users.noreply.github.com>
2025-10-09 21:32:30 +0200

    hm: move dotfiles module

--- home.nix
+++ home.nix
@@ -11,6 +11,23 @@
     ".latexmkrc".source = lib'.mkConfigPath "latexmkrc";
   };
 
+  oliwia.home = {
+    configSymlink = {
+      alacritty = "alacritty";
+      fastfetch = "fastfetch";
+      git = "git";
+      hypr = "hypr";
+      hyprpanel = "hyprpanel";
+      "oh-my-posh/config.json" = "oh-my-posh/omp-vague.json";
+      qimgv = "qimgv";
+      ripgrep = "ripgrep";
+      sioyek = "sioyek";
+      tmux = "tmux";
+      rofi = "rofi";
+    };
+  };
+  home.sessionVariables."RIPGREP_CONFIG_PATH" = "$HOME/.config/ripgrep/ripgreprc";
+
   home.sessionVariables = {
     EDITOR = "nvim";
     BROWSER = "zen";
