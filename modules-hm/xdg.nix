71efb1524b674ca5192efe5c82da24740e53bd04
oliwia <24637207+kkanden@users.noreply.github.com>
2025-10-29 22:58:08 +0100

    xdg: remove nvim-alacritty

--- modules-hm/xdg.nix
+++ modules-hm/xdg.nix
@@ -3,18 +3,6 @@
   xdg = {
     enable = true;
     desktopEntries = {
-      nvim-alacritty = {
-        name = "Neovim in Alacritty";
-        genericName = "Text Editor";
-        exec = "alacritty -e nvim %F";
-        icon = "alacritty";
-        mimeType = [ "text/plain" ];
-        terminal = false;
-        categories = [
-          "Utility"
-          "TextEditor"
-        ];
-      };
       nautilus = {
         name = "Nautilus";
         genericName = "File explorer";
