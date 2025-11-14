a691798f88c480be55a47b525b6c8f535d08a445
oliwia <24637207+kkanden@users.noreply.github.com>
2025-11-14 15:19:41 +0100

    nvim: remove weird parser linking stuff

--- modules-hm/neovim.nix
+++ modules-hm/neovim.nix
@@ -1,26 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
-
   programs.neovim = {
     enable = true;
     plugins = [
       pkgs.vimPlugins.nvim-treesitter.withAllGrammars
     ];
   };
-
-  # link treesitter parser to somewhere in vim.opt.rtp
-  home.activation =
-    let
-      grammarsPath = pkgs.symlinkJoin {
-        name = "nvim-treesitter-grammars";
-        paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
-      };
-    in
-    {
-      linkTreesitterParsers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        rm -rf $HOME/.local/share/nvim/parser
-        mkdir -p $HOME/.local/share/nvim/parser
-        ln -rs ${grammarsPath}/parser/* $HOME/.local/share/nvim/parser
-      '';
-    };
 }
