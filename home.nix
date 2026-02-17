cc26be60ae7c06570af855d329e4335728acb561
oliwia <24637207+kkanden@users.noreply.github.com>
2026-02-17 22:06:11 +0100

    add bat, fzf config

--- home.nix
+++ home.nix
@@ -20,11 +20,13 @@
   oliwia.home = {
     configSymlink = {
       alacritty = "alacritty";
+      bat = "bat";
       fastfetch = "fastfetch";
       clangd = "clangd";
       dust = "dust";
       "fish/theme.fish" = "fish/vague.fish";
       "fish/config.fish" = "fish/config.fish";
+      fzf = "fzf";
       git = "git";
       hypr = "hypr";
       hyprpanel = "hyprpanel";
@@ -37,6 +39,7 @@
     };
   };
   home.sessionVariables."RIPGREP_CONFIG_PATH" = "$HOME/.config/ripgrep/ripgreprc";
+  home.sessionVariables."FZF_DEFAULT_OPTS_FILE" = "$HOME/.config/fzf/config";
 
   # adds xdg variables
   xdg.enable = true;
