452d76a450155189818bd6e0ac7764f4ed6a0c2f
oliwia <24637207+kkanden@users.noreply.github.com>
2026-05-17 23:00:41 +0200

    flake.lock, remove hyprlandPlugins overlay

--- overlays.nix
+++ overlays.nix
@@ -5,21 +5,6 @@ final: prev: {
     config.allowUnfree = true;
   };
   zen = inputs.zen-browser.packages.${system}.default;
-  hyprlandPlugins = prev.hyprlandPlugins // {
-    csgo-vulkan-fix = prev.hyprlandPlugins.csgo-vulkan-fix.overrideAttrs {
-      version = "0.54.0";
-      src =
-        let
-          gh-src = prev.fetchFromGitHub {
-            owner = "hyprwm";
-            repo = "hyprland-plugins";
-            rev = "b85a56b9531013c79f2f3846fd6ee2ff014b8960";
-            hash = "sha256-xwNa+1D8WPsDnJtUofDrtyDCZKZotbUymzV/R5s+M0I=";
-          };
-        in
-        "${gh-src}/csgo-vulkan-fix";
-    };
-  };
   nix = prev.lixPackageSets.latest.lix;
   vague-gtk = prev.callPackage ./pkgs/vague-gtk.nix { };
   openldap = prev.openldap.overrideAttrs {
