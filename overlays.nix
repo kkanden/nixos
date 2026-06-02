bbc587095fd3bb40dc768c16cd1f8596008ec4fb
oliwia <24637207+kkanden@users.noreply.github.com>
2026-06-02 22:46:29 +0200

    flake.lock, remove obsolete overlays

--- overlays.nix
+++ overlays.nix
@@ -7,15 +7,4 @@ final: prev: {
   zen = inputs.zen-browser.packages.${system}.default;
   nix = prev.lixPackageSets.latest.lix;
   vague-gtk = prev.callPackage ./pkgs/vague-gtk.nix { };
-  openldap = prev.openldap.overrideAttrs {
-    doCheck = !prev.stdenv.hostPlatform.isi686;
-  };
-  wayle = prev.wayle.overrideAttrs {
-    patches = [
-      (prev.fetchpatch {
-        url = "https://github.com/kkanden/wayle/commit/f1284f31df7e8a1f0ca789df47ab80371e0f553a.patch";
-        hash = "sha256-ftm1jXPD88HCfvUzdJHXzgfjgtadkYUjLFKomaiR0iM=";
-      })
-    ];
-  };
 }
