ab566c80598f42a2497e6f20a7bdcd90a8f9bbe8
oliwia <oliwia@kanden.me>
2026-06-15 19:51:46 +0200

    t450: pure server config

--- hosts/t450/configuration.nix
+++ hosts/t450/configuration.nix
@@ -9,52 +9,14 @@
   ]
   ++ lib.filesystem.listFilesRecursive ./imports;
 
-  oliwia = {
-    packages.extra = {
-      enable = true;
-      extraPackages = with pkgs; [ bluetui ];
+  oliwia.immich = {
+    server.enable = true;
+    backup = {
+      db.enable = true;
+      assets.enable = true;
     };
-    hyprland = {
-      enable = true;
-      extraConfig = /* lua */ ''
-        hl.config({
-          debug = {
-            disable_scale_checks = true
-          },
-          input = {
-            sensitivity = 0,
-            touchpad =  {
-              natural_scroll = true,
-              drag_lock = true,
-              scroll_factor = 0.7,
-            }
-          }
-        })
-      '';
-      monitors = [
-        {
-          name = "eDP-1";
-          scaling = "0.85";
-          main = true;
-        }
-      ];
-    };
-    theme.enable = true;
   };
 
-  services.playerctld.enable = true;
-  services.qbittorrent.enable = true;
-  services.mullvad-vpn.enable = true;
-  systemd.services.qbittorrent =
-    let
-      vpn-device = "sys-devices-virtual-net-wg0\\x2dmullvad.device";
-    in
-    {
-      after = lib.mkAfter [ vpn-device ];
-      bindsTo = lib.mkAfter [ vpn-device ];
-      upheldBy = lib.mkAfter [ vpn-device ];
-      wantedBy = lib.mkForce [ ];
-    };
   services.upower.enable = true;
   hardware.bluetooth = {
     enable = true;
@@ -65,14 +27,6 @@
 
   services.logind.settings.Login = {
     HandleLidSwitch = "ignore";
-  };
-
-  oliwia.immich = {
-    server.enable = true;
-    backup = {
-      db.enable = true;
-      assets.enable = true;
-    };
   };
 
   # this is just so caddy+cgit stops complaining about dubious ownership
