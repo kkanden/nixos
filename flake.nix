f33e820384e541c4df63d0ecff68c84a2531eff9
oliwia <oliwia@kanden.me>
2026-06-29 17:18:24 +0200

    flake.lock (website)

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     kanden-website = {
-      url = "git+https://git.kanden.me/website?ref=v0.2";
+      url = "git+https://git.kanden.me/website";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nh = {
