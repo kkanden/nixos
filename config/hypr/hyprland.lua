9bf42eccae280548aaa88912ebdfe8933c8a8eb3
oliwia <oliwia@kanden.me>
2026-07-11 18:55:50 +0200

    hyprland: disable splash text

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -281,6 +281,7 @@ hl.config({
 	misc = {
 		force_default_wallpaper = -1,
 		disable_hyprland_logo = true,
+		disable_splash_rendering = true,
 		middle_click_paste = false,
 		focus_on_activate = true,
 	},
